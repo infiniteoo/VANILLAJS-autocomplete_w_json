a225d558d990b69dc5eee025eab79402f9d87cf7
Troy Dorman <troydorman@gmail.com>
2021-08-21 20:31:43 -0600

    fitering out search results with regex & .filter

--- main.js
+++ main.js
@@ -7,5 +7,11 @@ search.addEventListener("input", () => searchStates(search.value));
 const searchStates = async (searchText) => {
   const res = await fetch("data.json");
   const states = await res.json();
-  console.log(states);
+
+  // get matches to current text input
+  let matches = states.filter((state) => {
+    const regex = new RegExp(`^${searchText}`, "gi");
+    return state.name.match(regex) || state.abbr.match(regex);
+  });
+  console.log(matches);
 };
